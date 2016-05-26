1a7bdde471d801535ffe1dd67d374a3b13aef2ad
Avrumi Friedman <info@lakewoodprogramming.com>
2016-05-26 13:06:38 -0400

    Initial commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HockeySignups.Data;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HockeySignupsDb db = new HockeySignupsDb(Settings1.Default.ConStr);
+            //var e = new Event {Date = DateTime.Today.AddDays(3), MaxPeople = 20};
+            //db.AddEvent(e);
+            //Console.WriteLine(e.Id);
+            db.GetEvents().ToList().ForEach(e =>
+            {
+                Console.WriteLine(e.Date + " " + e.MaxPeople + " " + e.Id);
+            });
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ HockeySignups.Web/Controllers/AdminController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HockeySignups.Data;
+using HockeySignups.Web.Models;
+
+namespace HockeySignups.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        private string _connectionString =
+            @"Data Source=.\sqlexpress;Initial Catalog=HockeySignup;Integrated Security=True";
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateEvent(DateTime date, int maxPeople)
+        {
+            var db = new HockeySignupsDb(_connectionString);
+            Event e = new Event {Date = date, MaxPeople = maxPeople};
+            db.AddEvent(e);
+            TempData["Message"] = "Event Successfully created, Id: " + e.Id;
+            return RedirectToAction("Index", "Hockey");
+        }
+
+        public ActionResult History()
+        {
+            var db = new HockeySignupsDb(_connectionString);
+            IEnumerable<EventWithPeople> events = db.GetEventsWithCount();
+            return View(events);
+        }
+
+        public ActionResult EventDetails(int id)
+        {
+            var db = new HockeySignupsDb(_connectionString);
+            Event e = db.GetEventById(id);
+            IEnumerable<EventSignup> signups = db.GetEventSignups(id);
+            var vm = new EventDetailsViewModel
+            {
+                Event = e,
+                Signups = signups
+            };
+            return View(vm);
+        }
+
+    }
+}
